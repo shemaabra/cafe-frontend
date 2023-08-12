7e63cf687f7849bb383bd93f66255789d723c1c8
shemaabra <abrashema9@gmail.com>
2023-08-12 14:39:40 +0300

    category module

--- src/app/material-component/dialog/category/category.component.ts
+++ src/app/material-component/dialog/category/category.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constant';
 
 @Component({
   selector: 'app-category',
@@ -6,12 +11,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss'],
 })
 export class CategoryComponent implements OnInit {
-
+  onAddCategory = new EventEmitter();
+  onEditCategory = new EventEmitter();
+  categoryForm: any = FormGroup;
+  dialogAction: any = 'Add';
+  action: any = 'add';
+  responseMessage: any;
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) public dialogData: any,
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    public dialogRef: MatDialogRef<CategoryComponent>,
+    private snackabarService: SnackbarService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categoryForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+    });
+    if (this.dialogAction === 'Edit') {
+      this.dialogAction = 'Edit';
+      this.action = 'Update';
+      this.categoryForm.patchValue(this.dialogData.data);
+    }
+  }
 
-
+  handleSubmit() {
+    if (this.dialogAction === 'Edit') {
+      this.edit();
+    } else {
+      this.add();
+    }
+  }
+  add() {
+    var formData = this.categoryForm.value;
+    var data = {
+      name: formData.name,
+    };
+    this.categoryService.add(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        this.onAddCategory.emit();
+        this.responseMessage = response.message;
+        this.snackabarService.openSnackBar(this.responseMessage, 'success');
+      },
+      (error: any) => {
+        this.dialogRef.close();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackabarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+  edit() {
+    var formData = this.categoryForm.value;
+    var data = {
+      id: this.dialogData.data.id,
+      name: formData.name,
+    };
+    this.categoryService.update(data).subscribe(
+      (response: any) => {
+        this.dialogRef.close();
+        this.onEditCategory.emit();
+        this.responseMessage = response.message;
+        this.snackabarService.openSnackBar(this.responseMessage, 'success');
+      },
+      (error: any) => {
+        this.dialogRef.close();
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackabarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
 }
