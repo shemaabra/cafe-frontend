86e624e814f9da62f89a76f6200b97bf8dbb6e97
shemaabra <abrashema9@gmail.com>
2023-08-24 14:56:54 +0300

    manage order done!

--- src/app/material-component/manage-order/manage-order.component.ts
+++ src/app/material-component/manage-order/manage-order.component.ts
@@ -5,6 +5,7 @@ import { CategoryService } from 'src/app/services/category/category.service';
 import { ProductService } from 'src/app/services/product/product.service';
 import { SnackbarService } from 'src/app/services/snackbar/snackbar.service';
 import { GlobalConstants } from 'src/app/shared/global-constant';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-manage-order',
@@ -110,18 +111,142 @@ export class ManageOrderComponent implements OnInit {
         this.price = response.price;
         this.manageOrderForm.controls['price'].setValue(response.price);
         this.manageOrderForm.controls['quantity'].setValue('1');
-        this.manageOrderForm.controls['total'].setValue(this.price*1);
+        this.manageOrderForm.controls['total'].setValue(this.price * 1);
       },
       (error: any) => {
         if (error.error?.message) {
           this.responseMessage = error.error?.message;
-        }else {
+        } else {
           this.responseMessage = GlobalConstants.genericError;
         }
-        this.responseMessage.openSnackBar(this.responseMessage, GlobalConstants.error)
+        this.responseMessage.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
       }
     );
   }
 
-  setQuantity(){}
+  setQuantity(value: any) {
+    var temp = this.manageOrderForm.controls['quantity'].value;
+    if (temp > 0) {
+      this.manageOrderForm.controls['total'].setValue(
+        this.manageOrderForm.controls['quantity'].value *
+          this.manageOrderForm.controls['price'].value
+      );
+    } else if (temp != '') {
+      this.manageOrderForm.controls['quantity'].setValue('1');
+      this.manageOrderForm.controls['total'].setValue(
+        this.manageOrderForm.controls['quantity'].value *
+          this.manageOrderForm.controls['price'].value
+      );
+    }
+  }
+
+  validateAddProduct() {
+    if (
+      this.manageOrderForm.controls['total'].value === 0 ||
+      this.manageOrderForm.controls['total'].value === null ||
+      this.manageOrderForm.controls['quantity'].value <= 0
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  validateSubmit() {
+    if (
+      this.totalAmount === 0 ||
+      this.manageOrderForm.controls['name'].value === null ||
+      this.manageOrderForm.controls['email'].value === null ||
+      this.manageOrderForm.controls['contactNumber'].value === null ||
+      this.manageOrderForm.controls['paymentMethod'].value === null ||
+      !(
+        this.manageOrderForm.controls['contactNumber'].valid ||
+        !this.manageOrderForm.controls['email'].valid
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  add() {
+    var formData = this.manageOrderForm.value;
+    var productName = this.dataSource.find(
+      (e: { id: number }) => e.id == formData.product.id
+    );
+    if (productName === undefined) {
+      this.totalAmount = this.totalAmount + formData.total;
+      this.dataSource.push({
+        id: formData.product.id,
+        name: formData.product.name,
+        category: formData.category.name,
+        quantity: formData.quantity,
+        price: formData.price,
+        total: formData.total,
+      });
+      this.dataSource = [...this.dataSource];
+      this._snackbarService.openSnackBar(
+        GlobalConstants.productAdded,
+        'success'
+      );
+    } else {
+      this._snackbarService.openSnackBar(
+        GlobalConstants.productExistError,
+        GlobalConstants.error
+      );
+    }
+  }
+
+  handleDeleteAction(value: any, element: any) {
+    this.totalAmount = this.totalAmount - element.total;
+    this.dataSource.splice(value, 1);
+    this.dataSource = [...this.dataSource];
+  }
+
+  submitAction() {
+    var formData = this.manageOrderForm.value;
+    var data = {
+      name: formData.name,
+      email: formData.email,
+      contactNumber: formData.contactNumber,
+      paymentMethod: formData.paymentMethod,
+      totalAmount: this.totalAmount,
+      productDetails: JSON.stringify(this.dataSource),
+    };
+    this._billService.generateReport(data).subscribe(
+      (response: any) => {
+        this.downloadFile(response?.uuid);
+        this.manageOrderForm.reset();
+        this.dataSource = [];
+        this.totalAmount = 0;
+      },
+      (error: any) => {
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this._snackbarService.openSnackBar(
+          this.responseMessage,
+          GlobalConstants.error
+        );
+      }
+    );
+  }
+
+  downloadFile(fileName: any) {
+    var data = {
+      uuid: fileName,
+    };
+    this._billService.getPDF(data).subscribe(
+      (response: any) => {
+        saveAs(response, fileName + '.pdf');
+      },
+      (error: any) => {
+        console.log(error.error?.message);
+      }
+    );
+  }
 }
